59e6b1f23e2d8a1d3a03beed4c0d06add12dfa51
Vlad Gerasimovich <vg.email.2@gmail.com>
2018-12-28 22:22:54 +0300

    add hero styles

--- gulp/tasks/images.minify.js
+++ gulp/tasks/images.minify.js
@@ -4,6 +4,6 @@ module.exports = function() {
     $.gulp.task('images:minify', function() {
         return $.gulp.src($.config.dev + '/images/**/*.*', { since: $.gulp.lastRun('images:minify') })
             .pipe($.gp.imagemin())
-            .pipe($.gulp.dest($.config.build + '/images'));
+            .pipe($.gulp.dest($.config.dev + '/images'));
     });
 };
